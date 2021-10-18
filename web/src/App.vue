6bee65aafade0d382544bd717cf37efd5d995762
WaleyChAn <zerogeneral@foxmail.com>
2021-10-18 17:33:40 +0800

    修改console逻辑

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,6 +3,15 @@
     <router-view />
   </div>
 </template>
+<script>
+import logs from './plugin/logs'
+
+export default {
+  created () {
+    logs()
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 </style>
--- web/src/main.js
+++ web/src/main.js
@@ -3,16 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import http from './plugin/http'
-import logs from './plugin/logs'
 import '@/assets/css/font-awesome.css'
 import '@/assets/css/site.scss'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = http
-
-router.afterEach(() => {
-  logs()
-})
 
 new Vue({
   router,
